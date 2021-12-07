38886a35e6d923a32763ae5884101fde2792a313
saramsv <sara.mousavi.ch@gmail.com>
2021-12-07 13:27:59 -0600

    first commit

---
+++ download-movie-reviews.sh
@@ -0,0 +1,5 @@
+#pip install kaggle
+#ls ~/.kaggle/
+#download the credentials and put in .kaggle to be authenticated mv kaggle.json ~/.kaggle/
+#kaggle datasets list -s 'IMDB'
+kaggle datasets download -d lakshmi25npathi/imdb-dataset-of-50k-movie-reviews
