4880881d90eccc377f126db5f4f0a487f281f6a1
saramsv <sara.mousavi.ch@gmail.com>
2021-12-07 13:40:35 -0600

    added some future works to the notbook

--- download-movie-reviews.sh
+++ download-movie-reviews.sh
@@ -1,5 +1,10 @@
-#pip install kaggle
-#ls ~/.kaggle/
-#download the credentials and put in .kaggle to be authenticated mv kaggle.json ~/.kaggle/
-#kaggle datasets list -s 'IMDB'
+# Here I am using the kaggle API through CLI for downloading the data
+# 1. Install the kaggle API:  pip install kaggle
+# Here I am using the kaggle API through CLI for downloading the data
+# 1. Install the kaggle API:  pip install kaggle
+# 2. check if this file is created: ls ~/.kaggle/
+# 3. download the credentials from your kaggle account and put them in .kaggle to be authenticated: mv kaggle.json ~/.kaggle/
+# 4. check the existing datasets: kaggle datasets list -s 'IMDB'
+# 5. download the desired dataset:
 kaggle datasets download -d lakshmi25npathi/imdb-dataset-of-50k-movie-reviews
+mv lakshmi25npathi/imdb-dataset-of-50k-movie-reviews data/
